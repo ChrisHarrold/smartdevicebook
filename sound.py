de005209fe273c96cdf9f252e3a3d041b17fdb72
charrold303 <christophereharrold@hotmail.com>
2019-10-29 13:16:37 -0600

    added try except for ctrl-c break

--- sound.py
+++ sound.py
@@ -20,18 +20,42 @@ GPIO.setup(green_led, GPIO.OUT)
 #turn on the green LED so you know we are working
 GPIO.output(green_led, GPIO.HIGH)
 
-# Define a listener on the sound sensor to wait for a signal - this will cycle the LED as sound comes in
-def callback(sensor_in):
-        if GPIO.input(sensor_in):
-                print "Sound detected - turn on"
-                GPIO.output(red_led, GPIO.HIGH)
-        else:
-                print "Sound detected - turn off"
-                GPIO.output(red_led, GPIO.LOW)
+try:
+    # Define a listener on the sound sensor to wait for a signal - this will cycle the LED as sound comes in
+    def callback(sensor_in):
+            if GPIO.input(sensor_in):
+                    print "Sound detected - turn on"
+                    GPIO.output(red_led, GPIO.HIGH)
+            else:
+                    print "Sound detected - turn off"
+                    GPIO.output(red_led, GPIO.LOW)
 
-GPIO.add_event_detect(sensor_in, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(sensor_in, callback)  # assign function to GPIO PIN, Run function on change
+    GPIO.add_event_detect(sensor_in, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+    GPIO.add_event_callback(sensor_in, callback)  # assign function to GPIO PIN, Run function on change
 
-# infinite loop
-while True:
-        time.sleep(1)
+    # infinite loop
+    while True:
+            time.sleep(1)
+
+except (KeyboardInterrupt, SystemExit):
+	
+	#If the system is interrupted (ctrl-c) this will print the final values
+	#so that you have at least some idea of what happened
+	print "-------------------------------------------"
+	print " "
+	print "System Reset on Keyboard Command or SysExit"
+	print " "
+	print "-------------------------------------------"
+	GPIO.cleanup()
+
+else:
+	
+	GPIO.cleanup()
+
+        # You can remove this entire block once you go to "production" mode
+		# but these values are critical for the initial tuning phase.
+        print "-------------------------------------------"
+        print " "
+        print "System Reset on Keyboard Command or SysExit"
+        print " "
+        print "-------------------------------------------"
